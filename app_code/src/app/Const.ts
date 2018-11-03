7bc51a13c7c63813b90fb64a8e9435353c91d182
chenchutian <447438519@qq.com>
2018-11-03 18:28:26 +0800

    添加选项

--- app_code/src/app/Const.ts
+++ app_code/src/app/Const.ts
@@ -1,6 +1,10 @@
 class Const {
-
+    //正式版本----------------------------------------
     public static GAME_HOST:string = "47.94.144.133";
     public static GAME_PORT:string = "9081";
     public static httpUrl:string = "http://47.94.144.133:8888";
+    // //测试服----------------------------------------
+    // public static GAME_HOST:string = "192.168.1.36";
+    // public static GAME_PORT:string = "9081";
+    // public static httpUrl:string = "http://192.168.1.36";
 }
