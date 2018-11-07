23e7fb79564cca7fe0200bdaf6ad013a5bff8069
chenchutian <447438519@qq.com>
2018-11-07 19:30:27 +0800

    协议

--- app_code/src/app/WebSockeAddress.ts
+++ app_code/src/app/WebSockeAddress.ts
@@ -1,9 +1,22 @@
 class WebSockeAddress {
+    //---------------------------------------请求发送数据
 	/**加入房间 */
     public static joinroom:string = "joinroom";
 	/**开始牛牛游戏*/
     public static niuniu_start:string = "niuniu_start";
+	/**抢庄*/
+    public static niuniu_dohog:string = "niuniu_dohog";
+	/**投注倍数*/
+    public static erbagang_bet:string = "erbagang_bet"
+    //---------------------------------------回包监听数据
 	/**监听抢庄按钮*/
     public static hog:string = "hog";
-    
+    /**用户抢庄数*/
+    public static hognum:string = "hognum";  
+    /**庄家*/
+    public static banker:string = "banker";  
+    /**倍数列表*/
+    public static bet:string = "bet";  
+    /**用户倍数*/
+    public static betnum:string = "betnum";  
 }
