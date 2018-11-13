b2e60b5ab4e527571bef914a27f808a4f5705e72
xiefeixf <936482519@qq.com>
2018-11-13 19:39:29 +0800

    111

--- app_code/src/app/ReceiveMessageMgr.ts
+++ app_code/src/app/ReceiveMessageMgr.ts
@@ -1,54 +1,53 @@
 module xlLib {
-    export class ReceiveMessageMgr {
-    private static _instance: ReceiveMessageMgr;
+	export class ReceiveMessageMgr {
+		private static _instance: ReceiveMessageMgr;
 
-	/**
-	 * http请求管理器单例
-	 */
-	public static getInstance(): ReceiveMessageMgr {
-		if (!this._instance) {
-			this._instance = new ReceiveMessageMgr();
+		/**
+		 * http请求管理器单例
+		 */
+		public static getInstance(): ReceiveMessageMgr {
+			if (!this._instance) {
+				this._instance = new ReceiveMessageMgr();
 			}
 			return this._instance;
 		}
-	public constructor() {
-	}
+		public constructor() {
+		}
 
-	public initPushMessage() {
-		xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.players,this.onMessage);
-		xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.newplayer,this.onMessage);
-        xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.hog,this.onMessage);
-        xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.hognum,this.onMessage);
-		xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.banker,this.onMessage);
-		xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.bet,this.onMessage);
-		xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.betnum,this.onMessage);
-		xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.cards,this.onMessage);
-		xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.summary,this.onMessage);
-	}
+		public initPushMessage() {
+			xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.players, this.onMessage);
+			xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.newplayer, this.onMessage);
+			xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.hog, this.onMessage);
+			xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.hognum, this.onMessage);
+			xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.banker, this.onMessage);
+			xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.bet, this.onMessage);
+			xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.betnum, this.onMessage);
+			xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.cards, this.onMessage);
+			xlLib.WebSocketMgr.getInstance().registerMsgHandler(EventConst.summary, this.onMessage);
+		}
 
-	private onMessage(msg:any):void
-	{
-		switch (msg.command) {
-			// case "EventConst.betnum":
-			    //处理逻辑----------
+		private onMessage(msg: any): void {
+			switch (msg.command) {
+				// case "EventConst.betnum":
+				//处理逻辑----------
 				//EventUtil.dispatchEvent(msg.command,msg);
 				// return ;
-			case EventConst.players:
-		   		console.log(msg);
-		   		UserInfo.getInstance().addPlayes(msg);
-			   	EventUtil.dispatchEvent(msg.command,msg);
-				return ;
-			case EventConst.newplayer:
-		   		console.log(msg);
-		   		// UserInfo.getInstance().addPlayes(msg);
-			   	// EventUtil.dispatchEvent(msg.command,msg);
-				return ;
-			default:
-			    console.log("收到服务器推送："+msg.command);
-				EventUtil.dispatchEvent(msg.command,msg);
-				return;
+				case EventConst.players:
+					console.log(msg);
+					UserInfo.getInstance().addPlayes(msg);
+					EventUtil.dispatchEvent(msg.command, msg);
+					return;
+				case EventConst.newplayer:
+					console.log(msg);
+					// UserInfo.getInstance().addPlayes(msg);
+					// EventUtil.dispatchEvent(msg.command,msg);
+					return;
+				default:
+					console.log("收到服务器推送：" + msg.command);
+					EventUtil.dispatchEvent(msg.command, msg);
+					return;
+			}
+
 		}
-		
-	}
 	}
 }
