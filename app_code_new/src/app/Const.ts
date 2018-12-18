be09908f1e6bd2ef484cf738f714b6230568e404
xiefeixf <936482519@qq.com>
2018-12-19 00:52:48 +0800

    dsadas

--- app_code_new/src/app/Const.ts
+++ app_code_new/src/app/Const.ts
@@ -1,12 +1,12 @@
 class Const {
     //正式版本----------------------------------------
-    // public static GAME_HOST:string = "47.52.138.199";
-    // public static GAME_PORT:string = "9081";
-    // public static httpUrl:string = "http://47.52.138.199:80";
+    public static GAME_HOST:string = "47.52.138.199";
+    public static GAME_PORT:string = "9081";
+    public static httpUrl:string = "http://47.52.138.199:80";
     // //测试服----------------------------------------
-      public static GAME_HOST: string = "192.168.1.7";
-      public static GAME_PORT: string = "9081";
-      public static httpUrl: string = "http://192.168.1.7";
+    //   public static GAME_HOST: string = "192.168.1.7";
+    //   public static GAME_PORT: string = "9081";
+    //   public static httpUrl: string = "http://192.168.1.7";
     // public static GAME_HOST:string = "192.168.1.49";
     // public static GAME_PORT:string = "9081";
     // public static httpUrl:string = "http://192.168.1.49";
